0bac21892a92e57d8377ff421235124ec40631fe
royanrp045 <royanristuprayoga@gmail.com>
2023-08-09 18:31:50 +0700

    menambahkan kode untuk mendapatkan masukan email dan password yang dimasukkan oleh pengguna

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -12,12 +12,14 @@ const inputPasswordElement = document.querySelector('#inputPassword');
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
-loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
+loginFormElement.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+
+  /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+     Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+     Jika tidak, maka panggil fungsi showPopUp().
+   */
 });
